0d65db69731492f8237c84fe58ffa5f8782976a3
Iann-rst <iannrst11@gmail.com>
2022-09-29 18:41:43 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Header } from "./components/Header"
+
 function App() {
 
   return (
     <div className="App">
-      E ai galera!!!
+      <Header />
+
+      <div id="about" className="h-screen pt-44 px-9 text-slate-400 bg-black">
+        Primeira Seção
+      </div>
     </div>
   )
 }
