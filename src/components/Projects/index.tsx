57ca83fffb906a2854b5d34d307d201d0e983908
Iann-rst <iannrst11@gmail.com>
2022-09-30 18:01:39 -0300

    feat: seção projetos

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,9 @@
+export function Projects() {
+  return (
+    <section id="projects" className='bg-[#ebebeb]'>
+      <div className="md:max-w-[1344px] md:mx-auto h-screen md:pt-20 pt-16 md:px-16 px-7 ">
+        Projetos
+      </div>
+    </section>
+  )
+}
