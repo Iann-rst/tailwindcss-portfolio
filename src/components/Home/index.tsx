44a857b7c4d9433872d9daba4564fae2ea0a5588
Iann-rst <iannrst11@gmail.com>
2022-09-30 18:01:27 -0300

    fix: home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,20 +2,22 @@ import Logo from '../../assets/logo-home.svg';
 
 export function Home() {
   return (
-    <section id="home" className="md:flex-row-reverse md:justify-between h-screen bg-black text-white md:pt-20 pt-16 md:px-10 px-7 flex flex-col items-center">
+    <section className='bg-white'>
+      <div id="home" className="md:max-w-[1344px] md:mx-auto md:flex-row-reverse md:justify-between h-screen  text-black md:pt-20 pt-16 md:px-10 px-7 flex flex-col items-center">
 
-      <div className="mt-10 md:mt-5">
-        <img className="md:w-[600px] md:h-[784px]" src={Logo} alt="" />
-      </div>
+        <div className="mt-10 md:mt-5">
+          <img className="md:w-[600px] md:h-[784px]" src={Logo} alt="" />
+        </div>
 
-      <div className="md:mt-0 md:gap-2 flex flex-col items-center gap-1 mt-2">
-        <span className="text-2xl">Meu nome é</span>
-        <h1 className="text-4xl font-bold text-stone-500">Iann Rodrigues</h1>
-        <span className="text-xl text-center">Desenvolvedor Front-end e Mobile</span>
+        <div className="md:mt-0 md:gap-2 flex flex-col items-center gap-1 mt-2">
+          <span className="text-2xl">Meu nome é</span>
+          <h1 className="text-4xl font-bold text-stone-500">Iann Rodrigues</h1>
+          <span className="text-xl text-center">Desenvolvedor Front-end e Mobile</span>
 
-        <button className="mt-2 bg-orange-400 w-72 h-16 rounded-md hover:bg-orange-300 font-bold">
-          FALE COMIGO
-        </button>
+          <button className="mt-2 bg-orange-400 w-72 h-16 rounded-md hover:bg-orange-300 font-bold">
+            FALE COMIGO
+          </button>
+        </div>
       </div>
     </section>
   )
