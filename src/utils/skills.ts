caf0ea9266522627901c532a6dbe82f4270fd33a
Iann-rst <iannrst11@gmail.com>
2022-09-30 18:00:43 -0300

    feat: skills

---
+++ src/utils/skills.ts
@@ -0,0 +1,17 @@
+
+import htmlLogo from '../assets/icones/html.svg';
+import javascriptLogo from '../assets/icones/javascript.svg';
+import cssLogo from '../assets/icones/CSS3.svg';
+import reactLogo from '../assets/icones/react.svg'
+import tailwindLogo from '../assets/icones/tailwindcss.png';
+import typescriptLogo from '../assets/icones/typescript.svg';
+
+export const skills = [
+  { name: 'HTML', svg: htmlLogo },
+  { name: 'CSS', svg: cssLogo },
+  { name: 'JavaScript', svg: javascriptLogo },
+  { name: 'Typescript', svg: typescriptLogo },
+  { name: 'React', svg: reactLogo },
+  { name: 'Tailwind CSS', svg: tailwindLogo },
+  { name: 'React Native', svg: reactLogo },
+]
