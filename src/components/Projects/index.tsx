20ee36f436e9ea63ba9f2d04fcd90c2c63c45126
Iann-rst <iannrst11@gmail.com>
2022-10-04 10:24:38 -0300

    feat: add projetos em slider

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,13 +4,44 @@ import { useKeenSlider } from 'keen-slider/react';
 import nlwEsportLogo from '../../assets/img-projetos/nlw-esport.svg';
 import originSixLogo from '../../assets/img-projetos/origin-six.svg';
 import rentX from '../../assets/img-projetos/rentX.svg';
-
+import goFinances from '../../assets/img-projetos/gofinances.svg';
+import proffy from '../../assets/img-projetos/proffy.svg';
+import rocketHelp from '../../assets/img-projetos/rocketHelp.svg';
 
 export function Projects() {
   const [ref] = useKeenSlider<HTMLDivElement>({
     loop: true,
   },
-
+    [
+      (slider) => {
+        let timeout: ReturnType<typeof setTimeout>
+        let mouseOver = false
+        function clearNextTimeout() {
+          clearTimeout(timeout)
+        }
+        function nextTimeout() {
+          clearTimeout(timeout)
+          if (mouseOver) return
+          timeout = setTimeout(() => {
+            slider.next()
+          }, 5000)
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+        slider.on("dragStarted", clearNextTimeout)
+        slider.on("animationEnded", nextTimeout)
+        slider.on("updated", nextTimeout)
+      },
+    ]
   );
   return (
     <section id="projects" className='bg-[#ebebeb]'>
@@ -20,28 +51,44 @@ export function Projects() {
         <div ref={ref} className="keen-slider mt-20 min-w-[320px] py-10">
 
           <div className="keen-slider__slide number-slide1 px-2 flex flex-col items-center">
-            <h1 className='text-3xl font-bold mb-4'>NLW eSports</h1>
+            <h1 className='text-base md:text-3xl font-bold text-center'>NLW eSports</h1>
             <a href="" target="_blank" title="NLW ESPORTS">
               < img className="min-w-[304px] w-auto h-80" src={nlwEsportLogo} alt="" />
             </a >
           </div >
 
           <div className="keen-slider__slide number-slide2 px-2 flex flex-col items-center">
-            <h1 className='text-3xl font-bold mb-4'>Origin Six</h1>
+            <h1 className='text-base md:text-3xl font-bold text-center'>Origin Six</h1>
             <a title="Origin Six" href="">
               <img className="min-w-[304px] w-auto h-80" src={originSixLogo} alt="" />
             </a>
           </div>
+
           <div className="keen-slider__slide number-slide3 flex flex-col items-center px-2">
-            <h1 className='text-3xl font-bold mb-4'>Projeto 3</h1>
+            <h1 className='text-base md:text-3xl font-bold text-center'>RentX - app para locação de carros.</h1>
             <a title="Origin Six" href="">
-              <img className="min-w-[304px] w-auto h-80" src={originSixLogo} alt="" />
+              <img className="min-w-[304px] w-auto h-80" src={rentX} alt="" />
             </a>
           </div>
+
           <div className="keen-slider__slide number-slide4 flex flex-col items-center px-2">
-            <h1 className='text-3xl font-bold mb-4'>Projeto 4</h1>
+            <h1 className='text-base md:text-3xl font-bold  text-center'>GoFinances - app de controle de gastos pessoais</h1>
             <a title="Origin Six" href="">
-              <img className="min-w-[304px] w-auto h-80" src={originSixLogo} alt="" />
+              <img className="min-w-[304px] w-auto h-80" src={goFinances} alt="" />
+            </a>
+          </div>
+
+          <div className="keen-slider__slide number-slide5 flex flex-col items-center px-2">
+            <h1 className='text-base md:text-3xl font-bold  text-center'>Proffy - Plataforma de estudos online</h1>
+            <a title="Origin Six" href="">
+              <img className="min-w-[304px] w-auto h-80" src={proffy} alt="" />
+            </a>
+          </div>
+
+          <div className="keen-slider__slide number-slide6 flex flex-col items-center px-2">
+            <h1 className='text-base md:text-3xl font-bold  text-center'>Rocket Help - Gerencie solicitações de suporte a equipamentos.</h1>
+            <a title="Origin Six" href="">
+              <img className="min-w-[304px] w-auto h-80" src={rocketHelp} alt="" />
             </a>
           </div>
         </div >
